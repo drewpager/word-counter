62529d56629a986330b495b718ceb3115f922005
Drew Page <drew@siegemedia.com>
2023-04-19 15:21:44 -0700

    Added some styling and started building general layout

--- src/components/wordCounter.jsx
+++ src/components/wordCounter.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './wordCounter.css';
 
 export const WordCounter = () => {
   const [wordCount, setWordCount] = useState(0);
@@ -11,11 +12,22 @@ export const WordCounter = () => {
   }
   return (
     <div>
-      <h2>Word Counter</h2>
+      <h2>Blog Text Readability Checker</h2>
+      <button className="reset-button">↻ Reset</button>
+      <div className="text--modifiers">
+        <p>Google Fonts</p>
+        <p>Text Size</p>
+        <p>Line Height</p>
+        <p>Text Color</p>
+        <p>Background Color</p>
+      </div>
       <h3>Count: {wordCount}</h3>
       <h3>Characters: {charCount}</h3>
-      <div>
+      <div className="text--area">
         <input type="text" className="text-area" onChange={(e) => handleChange(e)}/>
+      </div>
+      <div className="results--sidebar">
+
       </div>
     </div>
   )
