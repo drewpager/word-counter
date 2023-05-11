38f41639ea8ef2a5378277b8ed6f7532502f2e24
drewpager <drewpage@shredlights.com>
2023-05-11 16:51:10 -0700

    Progress on styling and functionality

--- src/components/wordCounter.jsx
+++ src/components/wordCounter.jsx
@@ -4,16 +4,40 @@ import './wordCounter.css';
 export const WordCounter = () => {
   const [wordCount, setWordCount] = useState(0);
   const [charCount, setCharCount] = useState(0);
+  const [sentenceCount, setsentenceCount] = useState(0);
+  const [active, setActive] = useState("px");
 
   const handleChange = (e) => {
     e.preventDefault();
     let length = e.target.value
+    let words = length.split(' ');
+    let sentences = length.split(/[.!?]/g);
     setCharCount(length.length);
+    setWordCount(words.length);
+    setsentenceCount(sentences.length - 1);
   }
+
+  const handleReset = () => {
+    setWordCount(0);
+    setCharCount(0);
+  }
+
+  // const handleMetric = (value) => {
+  //   setActive(value);
+  //   console.log(active)
+  // }
+
   return (
     <div>
-      <h2>Blog Text Readability Checker</h2>
-      <button className="reset-button">↻ Reset</button>
+      <h1 className="heading-text">Blog Text Readability Checker</h1>
+      <div className="button--div">
+        <div className="metric--selectors">
+          <button className={active === "px" ? "px active" : "px"} onClick={() => setActive("px")}>px</button>
+          <button className={active === "rem" ? "px active" : "px"} onClick={() => setActive("rem")}>rem</button>
+          <button className={active === "em" ? "px active" : "px"} onClick={() => setActive("em")}>em</button>
+        </div>
+        <button className="reset-button" onClick={handleReset}>↻ Reset</button>
+      </div>
       <div className="text--modifiers">
         <p>Google Fonts</p>
         <p>Text Size</p>
@@ -21,8 +45,9 @@ export const WordCounter = () => {
         <p>Text Color</p>
         <p>Background Color</p>
       </div>
-      <h3>Count: {wordCount}</h3>
+      <h3>Words: {wordCount}</h3>
       <h3>Characters: {charCount}</h3>
+      <h3>Sentences: {sentenceCount}</h3>
       <div className="text--area">
         <input type="text" className="text-area" onChange={(e) => handleChange(e)}/>
       </div>
